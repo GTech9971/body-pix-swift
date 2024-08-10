520ee9bc6e033282df64e7d462519083838f5aa2
George <georgetech1984@gmail.com>
2024-08-10 22:06:54 +0900

    プロジェクト作成

---
+++ body-pix-swift/body-pix-swift/body_pix_swift.h
@@ -0,0 +1,18 @@
+//
+//  body_pix_swift.h
+//  body-pix-swift
+//
+//  Created by George Dambara on 2024/08/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for body_pix_swift.
+FOUNDATION_EXPORT double body_pix_swiftVersionNumber;
+
+//! Project version string for body_pix_swift.
+FOUNDATION_EXPORT const unsigned char body_pix_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <body_pix_swift/PublicHeader.h>
+
+
